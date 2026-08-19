0adcaa76de41b2ffee5c12e7956fdb9bead669b6
tonbiattack <4.0497724e+07+tonbiattack@users.noreply.github.com>
2026-08-19 12:33:37 +0000

    fix: withFile内でファイル内容を完全に読む

--- labs/haskell-lazy-io-resource-debug-lab/src/GreetingFile.hs
+++ labs/haskell-lazy-io-resource-debug-lab/src/GreetingFile.hs
@@ -1,6 +1,6 @@
 module GreetingFile (readGreeting) where
 
-import System.IO (IOMode (ReadMode), hGetContents, withFile)
+import System.IO (IOMode (ReadMode), hGetContents', withFile)
 
 readGreeting :: FilePath -> IO String
-readGreeting path = withFile path ReadMode hGetContents
+readGreeting path = withFile path ReadMode hGetContents'
