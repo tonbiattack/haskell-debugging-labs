31c9af1d0ad7019599ee8ce4edf01acb665257bc
tonbiattack <4.0497724e+07+tonbiattack@users.noreply.github.com>
2026-08-19 13:52:01 +0000

    fix: 平均計算を小数除算で行う

--- labs/haskell-integral-division-debug-lab/src/Average.hs
+++ labs/haskell-integral-division-debug-lab/src/Average.hs
@@ -2,4 +2,5 @@ module Average (mean) where
 
 mean :: [Int] -> Maybe Double
 mean [] = Nothing
-mean values = Just (fromIntegral (sum values `div` length values))
+mean values =
+  Just (fromIntegral (sum values) / fromIntegral (length values))
